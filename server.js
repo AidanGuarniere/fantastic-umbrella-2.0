913840e570a377b1bc25cbd426925d964b3b5d50
aidan <aidanguarniere@gmail.com>
2021-01-28 18:34:17 -0800

    new stylesheet

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // requirements
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -6,8 +7,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // activate routes
 app.use(routes);
